168a1ca542239d953ba2867eb9166aaa53cee47f
Sorachantha <xwsgbx@gmail.com>
2017-08-15 09:19:10 -0400

    meal photos now sit side-by-side, hover over brightens and enlarges photo

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import '../css/BasicSetup.css';
+import '../css/FoodMenu.css';
 import image1 from '../images/1.jpg';
 import image2 from '../images/2.jpg';
 import image3 from '../images/3.jpg';
