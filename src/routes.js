ee20adb3981a4bb7c1149ee3e84700a9840d87b8
Andrew.Easterling <Anddrew914@gmail.com>
2017-08-17 15:36:54 -0400

    making all routes load on home page for now

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,9 @@ import Signup from './components/signup'
 
 const routes = (
     <div>
-        <Route exact path='/' component={Header} />
-        <Route exact path='/' component={Section} />
-        <Route exact path='/' component={FoodMenu} />
+        <Route path='/' component={Header} />
+        <Route path='/' component={Section} />
+        <Route path='/' component={FoodMenu} />
         <Route path='/signup' component={Signup} />
 
     </div>
