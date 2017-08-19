7ca1bf7f60df3d0015bfe75e264a2da2eefddff0
Sorachantha <xwsgbx@gmail.com>
2017-08-19 12:01:00 -0400

    delete routes.js and move components back into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/header.js';
 import Section from './components/section.js'
+import FoodMenu from './components/FoodMenu.js';
 import './App.css';
-import Routes from './routes'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {Routes}
+      <div>
+        <Header/>
+        <Section/>
+        <FoodMenu/>
       </div>
     );
   }
