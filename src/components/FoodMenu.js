5285f9d73445734bf389104399ff12e129b4b632
Sorachantha <xwsgbx@gmail.com>
2017-08-19 13:19:17 -0400

    add tooltip to each meal

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../css/BasicSetup.css';
 import '../css/FoodMenu.css';
 import '../assets/vendor/css/tooltipster.bundle.min.css';
+import MappleToolTip from 'reactjs-mappletooltip';
 import QuestionMark from '../fonts/QuestionMark.png';
 import Plus from '../fonts/Plus.png';
 import image1 from '../images/1.jpg';
@@ -14,7 +15,10 @@ import image7 from '../images/7.jpg';
 import image8 from '../images/8.jpg';
 
 class FoodMenu extends Component{
+
   render(){
+    const mappleConfig = { shadow: true, tipPosition: 25 }
+
     return(
       <div className="section-meals">
         <div className="row">
@@ -25,63 +29,127 @@ class FoodMenu extends Component{
 
         <ul className="meals-showcase">
           <li>
-            <figure className="meal-photo">
-              <img src={image1} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image1} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image2} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image2} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image3} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image3} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image4} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image4} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
         </ul>
 
         <ul className="meals-showcase">
           <li>
-            <figure className="meal-photo">
-              <img src={image5} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip direction={'bottom'} {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image5} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak</h4>
+                <p>Wonderfully done</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image6} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image6} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image7} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image7} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
           <li>
-            <figure className="meal-photo">
-              <img src={image8} alt="meal-1"/>
-              <img src={QuestionMark} alt="info button"/>
-              <img src={Plus} alt="add meal to cart button"/>
-            </figure>
+            <MappleToolTip {...mappleConfig}>
+              <div>
+                <figure className="meal-photo">
+                  <img src={image8} alt="meal-1"/>
+                  <img src={QuestionMark} alt="info button"/>
+                  <img src={Plus} alt="add meal to cart button"/>
+                </figure>
+              </div>
+              <div>
+                <h4>Wonder Steak $15</h4>
+                <p>Prepared for you to eat!</p>
+              </div>
+            </MappleToolTip>
           </li>
         </ul>
       </div>
